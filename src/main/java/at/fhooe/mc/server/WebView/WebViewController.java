d7fd2dd7d2c201bee0ce2ef1f9f6fdd337a2d0ff
Reder <reder@mre.lap>
2020-02-10 13:24:58 +0100

    added images & thymialeaf if conditions

--- src/main/java/at/fhooe/mc/server/WebView/WebViewController.java
+++ src/main/java/at/fhooe/mc/server/WebView/WebViewController.java
@@ -1,11 +1,14 @@
 package at.fhooe.mc.server.WebView;
 
+import at.fhooe.mc.server.Data.HourlyWeatherForecast;
 import at.fhooe.mc.server.Services.Optimizer.Optimizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class WebViewController {
@@ -18,7 +21,8 @@ public class WebViewController {
 
     @GetMapping("/")
     public String indexView(Model model) {
-        model.addAttribute("solarPanels", optimizer.getSolarPanels());
+        ArrayList<HourlyWeatherForecast> weatherForecast = optimizer.getWeatherForecasts();
+        model.addAttribute("weatherForecast", weatherForecast);
         return "overview";
     }
 
